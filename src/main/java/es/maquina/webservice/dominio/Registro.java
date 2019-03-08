3a21a2d67861ed9ee91f192413db959301c72caa
MaQuiNa1995 <maquina1995@gmail.com>
2019-03-08 18:32:36 +0100

    arreglo de la clase Registro

--- src/main/java/es/maquina/webservice/dominio/Registro.java
+++ src/main/java/es/maquina/webservice/dominio/Registro.java
@@ -1,48 +1,42 @@
-package es.cic.christian.domain;
-
-import java.util.logging.Logger;
+package es.maquina.webservice.dominio;
 
 /**
- * @author Christian Muñoz Ason
- * @version 0.1.0
- * @since 1.8
+ * 
  */
 public class Registro {
 
-    private static final Logger LOG = Logger.getLogger(Registro.class.getName());
+	private int id;
+	private String fecha;
+	private String nombreUsuario;
 
-    private int id;
-    private String fecha;
-    private String nombreUsuario;
+	public Registro(int id, String nombreUsuario, String fecha) {
+		this.id = id;
+		this.nombreUsuario = nombreUsuario;
+		this.fecha = fecha;
+	}
 
-    public Registro(int id, String nombreUsuario, String fecha) {
-        this.id = id;
-        this.nombreUsuario = nombreUsuario;
-        this.fecha = fecha;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
 
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
 
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
+	public String getFecha() {
+		return fecha;
+	}
 
-    public String getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
-    }
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
 
 }
