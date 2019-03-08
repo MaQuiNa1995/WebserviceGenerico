2084ff425e638d2db26527a829ef8f5ad5b64e99
MaQuiNa1995 <maquina1995@gmail.com>
2019-03-08 21:03:05 +0100

    metido a constante las propiedades de hibernate

--- src/main/java/es/maquina/webservice/configuracion/Configuracion.java
+++ src/main/java/es/maquina/webservice/configuracion/Configuracion.java
@@ -44,6 +44,7 @@ public class Configuracion {
 	private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
 	private static final String ENTITYMANAGER_PACKAGES_TO_SCAN = "es.maquina.webservice.persistencia.dominio";
 	private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "org.hibernate.dialect.HSQLDialect";
+	private static final String PROPERTY_NAME_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
 
 	@Bean
 	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> customContainer() {
@@ -110,7 +111,7 @@ public class Configuracion {
 		properties.put(PROPERTY_NAME_HIBERNATE_JDBC_FETCH_SIZE, 2);
 		properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, Boolean.TRUE);
 		properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, "org.hibernate.dialect.MySQLDialect");
-		properties.put("hibernate.hbm2ddl.auto", "create-drop");
+		properties.put(PROPERTY_NAME_HBM2DDL_AUTO, "create-drop");
 		return properties;
 	}
 
