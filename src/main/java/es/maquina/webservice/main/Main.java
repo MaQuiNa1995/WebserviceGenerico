33ca261e7b87d6a68793f0773c08a48c98bad53d
Christian Muñoz Asón <cmunozas@everis.com>
2019-08-20 14:32:55 +0200

    Revert "mejora en javadoc y nombres"
    This reverts commit 8dfc8a2424d52a689652a8e82cf59433868d3787.

--- src/main/java/es/maquina/webservice/main/Main.java
+++ src/main/java/es/maquina/webservice/main/Main.java
@@ -1,38 +1,41 @@
 /*
  * Copyright 2016 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package es.maquina.webservice.main;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Clase main que inicia la aplicación
+ *
+ * @author MaQuina1995
+ */
+@SpringBootApplication
+@ComponentScan("es.maquina.webservice")
 public class Main {
 
-    public static void main(String... args) {
-	try (InputStream inputStream = new ByteArrayInputStream("holahola".getBytes(StandardCharsets.UTF_8))) {
-
-	    OutputStream a = new OutputStream();
-
-	} catch (Exception exception) {
-
-	    // Pero si ocurre una excepción devolvemos un 400
-	    LOGGER.warning("Se ha producido una excepción al subir el fichero al bucket mas info: "
-		    + exception.getMessage());
-	    respuesta = Response.status(400).build();
+	/**
+	 * Método que inicia la aplicación
+	 *
+	 * @param args Array de strings que se le pueden pasar al método en este caso no
+	 *             se usa
+	 */
+	public static void main(String... args) {
+		SpringApplication.run(Main.class);
 	}
-    }
 
 }
